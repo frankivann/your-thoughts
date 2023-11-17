6b121bbc81aad6e4f9b77fe98953cedc14669362
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-17 10:03:25 -0500

    add form to render thoughts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,50 @@
+import { useState } from 'react'
+import { type Thought } from './types'
 import './styles/app.css'
 
+const initialThoughts = [
+  {
+    id: '90fs',
+    timestamp: new Date().toISOString(),
+    value: 'my first thought',
+  },
+  {
+    id: '0auu',
+    timestamp: new Date().toISOString(),
+    value: 'i love programming',
+  },
+]
+
 export default function App() {
+  const [thoughts, setThoughts] = useState<Thought[]>(initialThoughts)
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    /**
+     * Get input value
+     */
+
+    const form = event.target as HTMLFormElement
+    const formData = new FormData(form)
+    const thought = formData.get('thought') as string
+
+    /**
+     * Set new obj to thougts
+     */
+
+    const newThought = {
+      id: crypto.randomUUID(),
+      timestamp: new Date().toISOString(),
+      value: thought,
+    }
+
+    const newThoughts = [...thoughts, newThought]
+    setThoughts(newThoughts)
+
+    form.reset()
+  }
+
   return (
     <div className='app'>
       <header className='header'>
@@ -11,12 +55,16 @@ export default function App() {
       <main className='main'>
         <h1 className='title'>Give me your thoughts</h1>
 
-        <div>
-          <input type='text' />
-        </div>
+        <form onSubmit={onSubmit}>
+          <input type='text' name='thought' />
+        </form>
 
         <ul className='toughts'>
-          <li className='tought'>My first tought</li>
+          {thoughts.map(({ id, value }) => (
+            <li key={id} className='tought'>
+              {value}
+            </li>
+          ))}
         </ul>
       </main>
     </div>
