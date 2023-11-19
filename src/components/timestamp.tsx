88e0bc8f8a1fdd37df555e53723d76ab3f4e9bf8
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-19 16:38:42 -0500

    fix logic

--- src/components/timestamp.tsx
+++ src/components/timestamp.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { differenceInSeconds, getHours, getMinutes } from 'date-fns'
+import { differenceInSeconds, lightFormat } from 'date-fns'
 
 export function Timestamp({ timestamp }: { timestamp: string }) {
   const [seconds, setSeconds] = useState(0)
@@ -21,8 +21,8 @@ export function Timestamp({ timestamp }: { timestamp: string }) {
 
   const time = () => {
     if (hours > 24) {
-      const hour = getHours(new Date(timestamp))
-      const minutes = getMinutes(new Date(timestamp))
+      const hour = lightFormat(new Date(timestamp), 'HH')
+      const minutes = lightFormat(new Date(timestamp), 'mm')
 
       return `${hour}:${minutes}`
     } else if (hours > 0) return `${hours}h ago`
