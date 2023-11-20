b11605ffb23359f3f9be620ec8de6338a1bbe871
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-20 09:31:32 -0500

    add thoughts-per-day component

---
+++ src/components/thoughts-per-day.tsx
@@ -0,0 +1,26 @@
+import { type Thought } from '../types'
+import { ThoughtsListPerDay } from './thoughts-list-per-day'
+
+interface Props {
+  thoughtsPerDay: Thought[]
+  day: string
+  deleteThoughtById: (day: string, id: string) => void
+}
+
+export function ThoughtsPerDay({
+  thoughtsPerDay,
+  day,
+  deleteThoughtById,
+}: Props) {
+  return (
+    <div>
+      <h2>{day}</h2>
+      <ThoughtsListPerDay
+        key={crypto.randomUUID()}
+        day={day}
+        thoughtsPerDay={thoughtsPerDay}
+        deleteThoughtById={deleteThoughtById}
+      />
+    </div>
+  )
+}
