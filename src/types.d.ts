ddf4477fbe399fe021a7db57eb770c2b853b92ef
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-20 15:39:06 -0500

    declare global interface (idk what I'm doing lol)

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,18 @@
+/* Esto me lo acabo de inventar jajajaka */
+declare global {
+  interface Object {
+    groupBy(
+      items: Iterable,
+      callbackFn: (element: Iterable, index: number) => Iterable
+    ): { [key: string]: Iterable }
+  }
+}
+
 export interface Thought {
   id: string
   timestamp: string
   value: string
+  formatDay?: string
 }
 
 export interface Thoughts {
