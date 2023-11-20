f7a8b9e0f6064996ae1734255adb7edf7d5942ff
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-20 15:39:29 -0500

    refactor component

--- src/hooks/use-thoughts.tsx
+++ src/hooks/use-thoughts.tsx
@@ -1,7 +1,7 @@
 import { type Thoughts } from '../types'
 import { useState } from 'react'
+import { INITIAL_THOUGHTS } from '../constants'
 import { getStoredThoughts, storeThoughts } from '../services/thoughts'
-// import { format, isToday, isYesterday } from 'date-fns'
 
 export function useThoughts() {
   const [thoughts, setThoughts] = useState<Thoughts>(getStoredThoughts())
@@ -20,26 +20,9 @@ export function useThoughts() {
   }
 
   const deteleAllThoughts = () => {
-    setThoughts({ Today: [] })
-    storeThoughts({ Today: [] })
+    setThoughts(INITIAL_THOUGHTS)
+    storeThoughts(INITIAL_THOUGHTS)
   }
-
-  // const order = () => {
-  //   // console.log(thoughts)
-  //   const mapped = thoughts.map(thought => ({
-  //     ...thought,
-  //     createdAt: format(new Date(thought.timestamp), 'EEEE do'),
-  //   }))
-
-  //   const based = Object.groupBy(mapped, (thought: Thought) => {
-  //     if (isToday(new Date(thought.timestamp))) return 'Today'
-  //     else if (isYesterday(new Date(thought.timestamp))) return 'Yesterday'
-  //     return thought.createdAt
-  //   })
-  //   // console.log(based)
-  // }
-
-  // order()
 
   return {
     thoughts,
