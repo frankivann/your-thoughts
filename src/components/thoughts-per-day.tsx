6f0264d994c3ed7f28b5e970e5513877dd031279
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-22 15:33:14 -0500

    add children prop

--- src/components/thoughts-per-day.tsx
+++ src/components/thoughts-per-day.tsx
@@ -4,23 +4,26 @@ import { ThoughtsListPerDay } from './thoughts-list-per-day'
 interface Props {
   thoughtsPerDay: Thought[]
   day: string
+  children?: React.ReactNode
   deleteThoughtById: (day: string, id: string) => void
 }
 
 export function ThoughtsPerDay({
   thoughtsPerDay,
   day,
+  children,
   deleteThoughtById,
 }: Props) {
   return (
-    <div>
+    <article className='thoughts-per-day'>
       <h2>{day}</h2>
+      {children}
       <ThoughtsListPerDay
         key={day}
         day={day}
         thoughtsPerDay={thoughtsPerDay}
         deleteThoughtById={deleteThoughtById}
       />
-    </div>
+    </article>
   )
 }
