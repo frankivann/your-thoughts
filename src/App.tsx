1a5037824c5021d5c529376c3eb06b328933d6b5
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-22 15:31:23 -0500

    refactor app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
 import { useThoughts } from './hooks/use-thoughts'
 import { ThoughtsList } from './components/thoughts-list'
-import { Form } from './components/form'
 import './styles/app.css'
 
 export default function App() {
-  const { thoughts, updateThoughts, deleteThoughtById, deteleAllThoughts } =
-    useThoughts()
-
-  const handleDeleteAll = () => {
-    deteleAllThoughts()
-  }
+  const { thoughts, updateThoughts, deleteThoughtById } = useThoughts()
 
   return (
     <div className='app'>
       <header className='header'>
-        <p className='logo'>Logo</p>
+        <span className='logo'>Logo</span>
         <button>Logo</button>
       </header>
 
       <main className='main'>
-        <button onClick={handleDeleteAll}>delete all</button>
-        <h1 className='title'>Give me your thoughts</h1>
-
-        <Form thoughts={thoughts} updateThoughts={updateThoughts} />
-
         <ThoughtsList
           thoughts={thoughts}
+          updateThoughts={updateThoughts}
           deleteThoughtById={deleteThoughtById}
         />
       </main>
