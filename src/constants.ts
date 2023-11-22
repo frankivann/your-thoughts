d8ce8078362f1f23e8c01d5a8300451713c0b585
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-22 14:33:45 -0500

    replace enum to const

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const THOUGHTS_STORE_NAME = '_THOUGHTS_'
 export const INITIAL_THOUGHTS = { Today: [] }
-export enum KEY_DAYS {
-  TODAY = 'Today',
-  YESTERDAY = 'Yesterday',
-}
+export const KEY_DAYS = {
+  TODAY: 'Today',
+  YESTERDAY: 'Yesterday',
+} as const
