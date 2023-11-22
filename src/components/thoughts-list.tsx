21c0da49f12f4e6bce2a888a41ca04d1597e3704
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-22 15:33:04 -0500

    man I'm trying to do my best, don't judge me

--- src/components/thoughts-list.tsx
+++ src/components/thoughts-list.tsx
@@ -1,28 +1,52 @@
 import { KEY_DAYS } from '../constants'
 import { type Thoughts } from '../types'
+import { Form } from './form'
 import { ThoughtsPerDay } from './thoughts-per-day'
 
 interface Props {
   thoughts: Thoughts
   deleteThoughtById: (day: string, id: string) => void
+  updateThoughts: (newThoughts: Thoughts) => void
 }
 
-export function ThoughtsList({ thoughts, deleteThoughtById }: Props) {
+export function ThoughtsList({
+  thoughts,
+  deleteThoughtById,
+  updateThoughts,
+}: Props) {
   const entries = Object.entries(thoughts)
 
   return (
-    <section className='thoughts'>
-      {entries.map(([day, thoughts]) => {
-        // un poquito de magia que no me representa :(
-        if (day !== KEY_DAYS.TODAY && thoughts.length === 0) return
-        return (
-          <ThoughtsPerDay
-            key={day}
-            day={day}
-            thoughtsPerDay={thoughts}
-            deleteThoughtById={deleteThoughtById}
-          />
-        )
+    <section className='thoughts-list'>
+      {entries.map(([day, thoughtsPerDay]) => {
+        let children = null
+        const isToday = day === KEY_DAYS.TODAY
+        const isEmpty = thoughtsPerDay.length === 0
+
+        if (isToday) {
+          children = (
+            <ThoughtsPerDay
+              key={day}
+              day={day}
+              thoughtsPerDay={thoughtsPerDay}
+              deleteThoughtById={deleteThoughtById}
+            >
+              <Form thoughts={thoughts} updateThoughts={updateThoughts} />
+            </ThoughtsPerDay>
+          )
+        } else if (!isToday && isEmpty) return
+        else {
+          children = (
+            <ThoughtsPerDay
+              key={day}
+              day={day}
+              thoughtsPerDay={thoughtsPerDay}
+              deleteThoughtById={deleteThoughtById}
+            />
+          )
+        }
+
+        return children
       })}
     </section>
   )
