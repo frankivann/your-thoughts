a0f18985f24ea1174fdd17a4e0881ae32be7e797
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-27 15:03:30 -0500

    feat auto height based on content

--- src/components/thought.tsx
+++ src/components/thought.tsx
@@ -2,6 +2,7 @@ import { isToday } from 'date-fns'
 import { type Thought } from '../types'
 import { DynamicTimestamp } from './dynamic-timestamp'
 import { StaticTimestamp } from './static-timestamp'
+import { useEffect } from 'react'
 
 interface Props {
   thought: Thought
@@ -10,10 +11,21 @@ interface Props {
 export function Thought({ thought }: Props) {
   const today = isToday(new Date(thought.timestamp))
 
+  useEffect(
+    function () {
+      const element = document.getElementById(thought.id)
+      if (element) {
+        element.style.height = 'auto'
+        element.style.height = `${element.scrollHeight}px`
+      }
+    },
+    [thought.id]
+  )
+
   return (
     <li className='thought'>
       <textarea
-        data-thought-id={thought.id}
+        id={thought.id}
         rows={1}
         spellCheck={false}
         readOnly
