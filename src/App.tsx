b7e9ce9fe48c1f7b01e6dcbc90217a2edc508df3
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-30 17:13:50 -0500

    feat dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,89 @@ export default function App() {
   return (
     <div className='app'>
       <header className='header'>
-        <span className='logo'>Logo</span>
-        <button>Logo</button>
+        <span className='logo'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='30'
+            height='30'
+            viewBox='0 0 24 24'
+            strokeWidth='1.7'
+            stroke='currentColor'
+            fill='none'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+            <path d='M9.695 12.985a5.972 5.972 0 0 0 -3.295 -.985c-1.257 0 -2.436 .339 -3.4 1a9 9 0 1 1 18 0c-.966 -.664 -2.14 -1 -3.4 -1a6 6 0 0 0 -5.605 8.144' />
+          </svg>
+        </span>
+        <nav className='menu'>
+          <button className='toggle'>
+            <svg
+              fill='none'
+              width='24'
+              height='24'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16m-7 6h7'
+              ></path>
+            </svg>
+          </button>
+          <div className='dropdown-wrapper'>
+            <ul className='menu-ul'>
+              <li className='menu-li'>
+                <button className='menu-button'>
+                  Toggle Theme
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='16'
+                    height='16'
+                    className='svg-logo'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    fill='none'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                    <path d='M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z' />
+                  </svg>
+                </button>
+              </li>
+              <hr className='menu-divider' />
+              <li className='menu-li'>
+                <button className='menu-button'>
+                  Clear Thoughts
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='16'
+                    height='16'
+                    className='svg-logo'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    fill='none'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                    <path d='M21 16.008v-8.018a1.98 1.98 0 0 0 -1 -1.717l-7 -4.008a2.016 2.016 0 0 0 -2 0l-7 4.008c-.619 .355 -1 1.01 -1 1.718v8.018c0 .709 .381 1.363 1 1.717l7 4.008a2.016 2.016 0 0 0 2 0l7 -4.008c.619 -.355 1 -1.01 1 -1.718z' />
+                    <path d='M12 22v-10' />
+                    <path d='M12 12l8.73 -5.04' />
+                    <path d='M3.27 6.96l8.73 5.04' />
+                  </svg>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
 
       <ThoughtsList
