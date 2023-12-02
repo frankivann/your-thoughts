0063b7c51ab68f49dc182c44954466747a76b252
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-02 11:06:11 -0500

    add new constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,13 @@
-export const THOUGHTS_STORE_NAME = '_THOUGHTS_'
 export const INITIAL_THOUGHTS = { Today: [] }
+export const THOUGHTS_STORE_NAME = '_THOUGHTS_'
+export const THOUGHTS_THEME_NAME = '_THOUGHTS_THEME_'
+
 export const KEY_DAYS = {
   TODAY: 'Today',
   YESTERDAY: 'Yesterday',
+} as const
+
+export const THEME_MODES = {
+  DARK: 'dark',
+  LIGHT: 'light',
 } as const
