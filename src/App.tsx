0d291a56be020a7ff23de6230b78004a80c76a8e
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-02 11:06:01 -0500

    refactor app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useThoughts } from './hooks/use-thoughts'
 import { ThoughtsList } from './components/thoughts-list'
-import './styles/app.css'
 import { Header } from './components/header'
+import { LogoIcon } from './components/icons'
+import { DropdownMenu } from './components/dropdown-menu'
+import './styles/app.css'
 
 export default function App() {
-  const { thoughts, updateThoughts, deleteThoughtById } = useThoughts()
+  const { thoughts, updateThoughts, deleteThoughtById, deteleAllThoughts } =
+    useThoughts()
 
   return (
     <div className='app'>
-      <Header />
+      <Header>
+        <LogoIcon />
+        <DropdownMenu deteleAllThoughts={deteleAllThoughts} />
+      </Header>
 
       <ThoughtsList
         thoughts={thoughts}
