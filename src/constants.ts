ba4100606fefc531e1d9002ce906d379c4be7d58
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-03 14:34:03 -0500

    add keydown theme constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 export const INITIAL_THOUGHTS = { Today: [] }
 export const THOUGHTS_STORE_NAME = '_THOUGHTS_'
 export const THOUGHTS_THEME_NAME = '_THOUGHTS_THEME_'
+export const KEYDOWN_THEME = ['T', 't']
 
 export const KEY_DAYS = {
   TODAY: 'Today',
