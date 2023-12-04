4493a8a01ba339dae56a70ce4c2510d3ef8a026c
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-04 15:11:19 -0500

    add interface emoji picker select

--- src/types.d.ts
+++ src/types.d.ts
@@ -26,4 +26,15 @@ export interface Thoughts {
   [key: string]: Thought[]
 }
 
+export interface EmojiPickerSelect {
+  id: string
+  name: string
+  native: string
+  unified: string
+  keywords: string[]
+  shortcodes: string
+  skin: number
+  aliases: string[]
+}
+
 export type ThemeMode = (typeof THEME_MODES)[keyof typeof THEME_MODES]
