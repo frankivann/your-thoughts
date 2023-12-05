f0e08f5d0d23c681f082a29cad68957a1ba4eead
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-05 14:26:02 -0500

    refactor dropdown menu component

--- src/components/dropdown-menu.tsx
+++ src/components/dropdown-menu.tsx
@@ -1,35 +1,49 @@
-import { BoxIcon, HamburgerIcon } from './icons'
-import { useTheme } from '../hooks/use-theme'
+import { useEffect, useRef } from 'react'
+import { BoxIcon } from './icons'
 
 interface Props {
   deteleAllThoughts: () => void
+  closeDropdonwMenu: () => void
+  toggleTheme: () => void
 }
 
-export function DropdownMenu({ deteleAllThoughts }: Props) {
-  const { toggleTheme } = useTheme()
+export function DropdownMenu({
+  deteleAllThoughts,
+  closeDropdonwMenu,
+  toggleTheme,
+}: Props) {
+  const dropdownRef = useRef<HTMLUListElement | null>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const dropdown = dropdownRef.current
+      if (!dropdown) return
+
+      const isTarget = dropdown.contains(event.target as Node)
+      if (!isTarget) closeDropdonwMenu()
+    }
+
+    window.document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      window.document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [closeDropdonwMenu])
 
   return (
-    <nav className='menu'>
-      <button className='toggle'>
-        <HamburgerIcon />
-      </button>
-      <div className='dropdown-wrapper'>
-        <ul className='menu-ul'>
-          <li className='menu-li'>
-            <button className='menu-button' onClick={toggleTheme}>
-              Toggle Theme
-              <kbd>T</kbd>
-            </button>
-          </li>
-          <hr className='menu-divider' />
-          <li className='menu-li'>
-            <button className='menu-button' onClick={() => deteleAllThoughts()}>
-              Clear Thoughts
-              <BoxIcon />
-            </button>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <ul className='menu-ul' ref={dropdownRef}>
+      <li className='menu-li'>
+        <button className='menu-button' onClick={toggleTheme}>
+          Toggle Theme
+          <kbd>T</kbd>
+        </button>
+      </li>
+      <hr className='menu-divider' />
+      <li className='menu-li'>
+        <button className='menu-button' onClick={() => deteleAllThoughts()}>
+          Clear Thoughts
+          <BoxIcon />
+        </button>
+      </li>
+    </ul>
   )
 }
