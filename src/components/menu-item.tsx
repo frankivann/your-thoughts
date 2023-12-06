f79c5e782ace785898939bed4aa66d28ade19134
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-06 10:09:27 -0500

    feat menu item component

---
+++ src/components/menu-item.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  title: string
+  icon: React.ReactNode
+  onClick: () => void
+}
+
+export function MenuItem({ onClick, title, icon }: Props) {
+  return (
+    <li className='menu-li'>
+      <button className='menu-button' onClick={onClick}>
+        {title}
+        {icon}
+      </button>
+    </li>
+  )
+}
