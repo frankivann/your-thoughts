7a7ab8801fe146cb6caa0cd9f87c063c5b605a3c
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-06 10:09:02 -0500

    refactor dropdown menu component

--- src/components/dropdown-menu.tsx
+++ src/components/dropdown-menu.tsx
@@ -1,49 +1,7 @@
-import { useEffect, useRef } from 'react'
-import { BoxIcon } from './icons'
-
 interface Props {
-  deteleAllThoughts: () => void
-  closeDropdonwMenu: () => void
-  toggleTheme: () => void
+  children: React.ReactNode
 }
 
-export function DropdownMenu({
-  deteleAllThoughts,
-  closeDropdonwMenu,
-  toggleTheme,
-}: Props) {
-  const dropdownRef = useRef<HTMLUListElement | null>(null)
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      const dropdown = dropdownRef.current
-      if (!dropdown) return
-
-      const isTarget = dropdown.contains(event.target as Node)
-      if (!isTarget) closeDropdonwMenu()
-    }
-
-    window.document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      window.document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [closeDropdonwMenu])
-
-  return (
-    <ul className='menu-ul' ref={dropdownRef}>
-      <li className='menu-li'>
-        <button className='menu-button' onClick={toggleTheme}>
-          Toggle Theme
-          <kbd>T</kbd>
-        </button>
-      </li>
-      <hr className='menu-divider' />
-      <li className='menu-li'>
-        <button className='menu-button' onClick={() => deteleAllThoughts()}>
-          Clear Thoughts
-          <BoxIcon />
-        </button>
-      </li>
-    </ul>
-  )
+export function DropdownMenu({ children }: Props) {
+  return <ul className='menu-ul'>{children}</ul>
 }
