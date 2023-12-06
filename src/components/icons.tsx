06849c0917320153ce05f132b78b59b54a2b5fc8
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-06 10:09:17 -0500

    feat close icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -79,3 +79,23 @@ export function BoxIcon() {
     </svg>
   )
 }
+
+export function CloseIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='18'
+      height='18'
+      viewBox='0 0 24 24'
+      strokeWidth='2'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M18 6l-12 12' />
+      <path d='M6 6l12 12' />
+    </svg>
+  )
+}
