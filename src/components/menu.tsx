8fb8a08671a989c7a9c3c1082f95600312bf4218
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-06 10:09:35 -0500

    refactor menu component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,7 +1,10 @@
-import { HamburgerIcon } from './icons'
+import { useState, useEffect, useRef } from 'react'
 import { useTheme } from '../hooks/use-theme'
-import { useState } from 'react'
+import { useModal } from '../hooks/use-modal'
+import { Modal } from './modal'
+import { MenuItem } from './menu-item'
 import { DropdownMenu } from './dropdown-menu'
+import { BoxIcon, HamburgerIcon } from './icons'
 
 interface Props {
   deteleAllThoughts: () => void
@@ -9,7 +12,24 @@ interface Props {
 
 export function Menu({ deteleAllThoughts }: Props) {
   const { toggleTheme } = useTheme()
+  const { showModal, closeModal, openModal } = useModal()
   const [showDropdown, setShowDropdown] = useState(false)
+  const menuRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const menu = menuRef.current
+      if (!showDropdown || !menu) return
+
+      const isTarget = menu.contains(event.target as Node)
+      if (!isTarget) closeDropdonwMenu()
+    }
+
+    window.document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      window.document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [menuRef, showDropdown])
 
   const toggleDropdownMenu = () => {
     setShowDropdown(!showDropdown)
@@ -19,25 +39,46 @@ export function Menu({ deteleAllThoughts }: Props) {
     setShowDropdown(false)
   }
 
+  const handleClick = () => {
+    openModal()
+    closeDropdonwMenu()
+  }
+
   const backgroundColor = showDropdown ? 'var(--gray2)' : ''
 
   return (
-    <nav className='menu'>
-      <button
-        className='toggle'
-        onClick={toggleDropdownMenu}
-        style={{ backgroundColor }}
-      >
-        <HamburgerIcon />
-      </button>
+    <>
+      <nav className='menu' ref={menuRef}>
+        <button
+          className='toggle'
+          onClick={toggleDropdownMenu}
+          style={{ backgroundColor }}
+        >
+          <HamburgerIcon />
+        </button>
 
-      {showDropdown && (
-        <DropdownMenu
-          toggleTheme={toggleTheme}
-          closeDropdonwMenu={closeDropdonwMenu}
-          deteleAllThoughts={deteleAllThoughts}
-        />
-      )}
-    </nav>
+        {showDropdown && (
+          <DropdownMenu>
+            <MenuItem
+              title='Toggle Theme'
+              icon={<kbd>T</kbd>}
+              onClick={toggleTheme}
+            />
+            <hr className='menu-divider' />
+            <MenuItem
+              title='Clear Thoughts'
+              icon={<BoxIcon />}
+              onClick={handleClick}
+            />
+          </DropdownMenu>
+        )}
+      </nav>
+
+      <Modal
+        showModal={showModal}
+        closeModal={closeModal}
+        onAccept={deteleAllThoughts}
+      />
+    </>
   )
 }
