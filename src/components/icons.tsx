32f19a7c0a2277f42cb22a3cdaeca13274f1810a
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-08 15:12:39 -0500

    remove icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -37,26 +37,6 @@ export function HamburgerIcon() {
   )
 }
 
-export function MoonIcon() {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='16'
-      height='16'
-      className='svg-icon'
-      viewBox='0 0 24 24'
-      strokeWidth='1.5'
-      stroke='currentColor'
-      fill='none'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-      <path d='M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z' />
-    </svg>
-  )
-}
-
 export function BoxIcon() {
   return (
     <svg
