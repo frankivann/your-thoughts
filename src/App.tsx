43b9320777053f469bcf50491a8517cee3e13226
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-09 15:56:36 -0500

    add onboarding component and custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Menu } from './components/menu'
 import { Header } from './components/header'
 import { LogoIcon } from './components/icons'
 import { ThoughtsList } from './components/thoughts-list'
+import { Onboarding } from './components/onboarding'
+import { useOnboarding } from './hooks/use-onboarding'
 import './styles/app.css'
 
 export default function App() {
@@ -14,6 +16,9 @@ export default function App() {
     deteleAllThoughts,
   } = useThought()
 
+  const { hasOnboarded, onboardingThoughts, showContinue, completeOnboarding } =
+    useOnboarding()
+
   return (
     <div className='app'>
       <Header>
@@ -21,12 +26,20 @@ export default function App() {
         <Menu deteleAllThoughts={deteleAllThoughts} />
       </Header>
 
-      <ThoughtsList
-        thoughts={thoughts}
-        storeNewThought={storeNewThought}
-        createNewThought={createNewThought}
-        deleteThoughtById={deleteThoughtById}
-      />
+      {hasOnboarded ? (
+        <ThoughtsList
+          thoughts={thoughts}
+          storeNewThought={storeNewThought}
+          createNewThought={createNewThought}
+          deleteThoughtById={deleteThoughtById}
+        />
+      ) : (
+        <Onboarding
+          showContinue={showContinue}
+          onboardingThoughts={onboardingThoughts}
+          completeOnboarding={completeOnboarding}
+        />
+      )}
     </div>
   )
 }
