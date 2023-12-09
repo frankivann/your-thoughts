e3c3fd2c1db417d611a1c637e144c874ad36e94c
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-09 15:58:46 -0500

    feat 'use onboarding' custom hook

---
+++ src/hooks/use-onboarding.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+import { type Thought } from '../types'
+import { getStoredOnboarding, storeHasOnboarding } from '../services/onboarding'
+import { INITIAL_ONBOARDING_THOUGHTS } from '../constants'
+
+export function useOnboarding() {
+  const [hasOnboarded, setHasOnboarded] = useState(getStoredOnboarding())
+  const [onboardingThoughts, setOnboardingThoughts] = useState<Thought[]>([])
+  const [showContinue, setShowContinue] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(
+    function () {
+      const Allthoughts: NodeListOf<HTMLTextAreaElement> =
+        document.querySelectorAll('[data-thought-id]')
+
+      Allthoughts.forEach(element => {
+        element.style.height = 'auto'
+        element.style.height = `${element.scrollHeight}px`
+      })
+    },
+    [onboardingThoughts]
+  )
+
+  useEffect(
+    function () {
+      const intervalId = setInterval(() => {
+        if (currentIndex < INITIAL_ONBOARDING_THOUGHTS.length) {
+          const newThoughts = [
+            ...onboardingThoughts,
+            INITIAL_ONBOARDING_THOUGHTS[currentIndex],
+          ]
+
+          setOnboardingThoughts(newThoughts)
+          setCurrentIndex(prevIndex => prevIndex + 1)
+        } else {
+          setShowContinue(true)
+          clearInterval(intervalId)
+        }
+      }, 1500)
+
+      return () => clearInterval(intervalId)
+    },
+    [currentIndex, onboardingThoughts]
+  )
+
+  const completeOnboarding = () => {
+    setHasOnboarded(true)
+    storeHasOnboarding()
+  }
+
+  return {
+    onboardingThoughts,
+    showContinue,
+    hasOnboarded,
+    completeOnboarding,
+  }
+}
