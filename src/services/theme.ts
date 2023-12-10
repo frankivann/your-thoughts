919cacef521ca0a429a3f65eee91a2163c46792e
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-10 14:31:41 -0500

    refactor theme services logic

--- src/services/theme.ts
+++ src/services/theme.ts
@@ -1,19 +1,24 @@
 import { THEME_MODES, THOUGHTS_THEME_NAME } from '../constants'
 import { type ThemeMode } from '../types'
 
-export function getStoredThemeMode() {
-  const theme = window.localStorage.getItem(THOUGHTS_THEME_NAME) as ThemeMode
-  if (theme) return theme
-
+function getPrefersColorScheme() {
   const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  return isDark ? THEME_MODES.DARK : THEME_MODES.LIGHT
+}
 
-  if (isDark) {
-    storeThemeMode(THEME_MODES.DARK)
-    return THEME_MODES.DARK
-  } else {
-    storeThemeMode(THEME_MODES.LIGHT)
-    return THEME_MODES.LIGHT
+export function getStoredThemeMode() {
+  const theme = window.localStorage.getItem(THOUGHTS_THEME_NAME)
+  const isValidThemeMode =
+    theme === THEME_MODES.DARK || theme === THEME_MODES.LIGHT
+
+  if (isValidThemeMode) {
+    storeThemeMode(theme)
+    return theme
   }
+
+  const favoriteTheme = getPrefersColorScheme()
+  storeThemeMode(favoriteTheme)
+  return favoriteTheme
 }
 
 export function storeThemeMode(theme: ThemeMode) {
