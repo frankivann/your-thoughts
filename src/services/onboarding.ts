d11412a0a4b694583de238750e989535fca06c30
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-10 14:31:33 -0500

    refactor onboarding services logic

--- src/services/onboarding.ts
+++ src/services/onboarding.ts
@@ -2,9 +2,28 @@ import { THOUGHTS_ONBOARDED } from '../constants'
 
 export function getStoredOnboarding() {
   const hasOnboarded = window.localStorage.getItem(THOUGHTS_ONBOARDED)
-  return hasOnboarded ? (JSON.parse(hasOnboarded) as boolean) : false
+
+  if (!hasOnboarded) {
+    storeNoHasOnboarding()
+    return false
+  }
+
+  try {
+    const onboardingParsed = JSON.parse(hasOnboarded) as boolean
+    const isBoolean = typeof onboardingParsed === 'boolean'
+    if (!isBoolean) throw new Error()
+
+    return onboardingParsed
+  } catch {
+    storeNoHasOnboarding()
+    return false
+  }
 }
 
 export function storeHasOnboarding() {
   window.localStorage.setItem(THOUGHTS_ONBOARDED, JSON.stringify(true))
+}
+
+export function storeNoHasOnboarding() {
+  window.localStorage.setItem(THOUGHTS_ONBOARDED, JSON.stringify(false))
 }
