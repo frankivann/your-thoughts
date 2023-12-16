58074f20fe80990b4c967d886c37ab776cebced4
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-16 13:51:47 -0500

    remove global declaration

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,5 @@
 import { THEME_MODES } from './constants'
 
-/* Esto me lo acabo de inventar jajajaka */
-declare global {
-  interface Object {
-    groupBy(
-      items: Iterable,
-      callbackFn: (element: Iterable, index: number) => Iterable
-    ): { [key: string]: Iterable }
-  }
-}
-
 declare global {
   interface Array<T> {
     toSorted(compareFn?: (a: T, b: T) => number): T[]
